66b7ee66968e349856ed8ca4a3925a0405ababb4
unknown <edjjxx1@gmail.com>
2022-12-22 10:59:41 +0800

    create repo

---
+++ focus1.py
@@ -0,0 +1,113 @@
+from time import sleep
+
+import psutil
+import ctypes
+from ctypes import wintypes
+
+# for i in range(1, 100000000):
+#     pid = wintypes.DWORD()
+#     active = ctypes.windll.user32.GetForegroundWindow()
+#     active_window = ctypes.windll.user32.GetWindowThreadProcessId(active, ctypes.byref(pid))
+#
+#     pid = pid.value
+#     for item in psutil.process_iter():
+#         print(item.name())
+#         if pid == item.pid:
+#             print(item.name())
+#     sleep(1)
+
+from typing import Optional
+from ctypes import wintypes, windll, create_unicode_buffer
+
+
+def getForegroundWindowTitle() -> Optional[str]:
+    hWnd = windll.user32.GetForegroundWindow()
+    # print(hWnd)
+    length = windll.user32.GetWindowTextLengthW(hWnd)
+    buf = create_unicode_buffer(length + 1)
+    windll.user32.GetWindowTextW(hWnd, buf, length + 1)
+    if buf.value:
+        return buf.value
+    else:
+        return None
+
+
+for i in range(1, 100000000):
+    print()
+
+    active_window_name = getForegroundWindowTitle()
+    print(active_window_name)
+    active_window_name = active_window_name.replace('–', '-').replace('|', '-').split(' - ')[::-1]
+    print(active_window_name)
+
+    pid = wintypes.DWORD()
+    active = ctypes.windll.user32.GetForegroundWindow()
+    active_window = ctypes.windll.user32.GetWindowThreadProcessId(active, ctypes.byref(pid))
+
+
+    pid = pid.value
+    for item in psutil.process_iter():
+        # print(item.name())
+        if pid == item.pid:
+            print(item.name())
+    sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setStyleSheet("background: black")
+        self.setWindowOpacity(0.9)
+
+
+# if __name__ == '__main__':
+#     app = QApplication([])
+#
+#     mw = MainWindow()
+#     mw.showFullScreen()
+#
+#     ok = QMessageBox.question(None, 'Question', 'Question?')
+#     if ok == QMessageBox.Yes:
+#         print('User select Ok')
+#     else:
+#         print('User select No')
+#
+#     # Убираем затемнение
+#     mw.close()
+#
+#     app.exec()
+
+
+
+
+
+
+# importing the module
+# import screen_brightness_control as sbc
+#
+# # get current brightness value
+# prev = sbc.get_brightness()
+# print(prev)
+#
+# # set brightness to 50%
+# sbc.set_brightness(1)
+#
+# print(sbc.get_brightness())
+# sleep(2)
+# # set the brightness of the primary display to 75%
+# sbc.set_brightness(prev[0])
+#
+# print(sbc.get_brightness())
